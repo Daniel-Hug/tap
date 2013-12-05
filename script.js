342d1a3d8187d79d9a9b4e73ad389b10fb71d14c
Daniel Hug <danielthehug@gmail.com>
2013-12-04 16:58:39 -0800

    Add all of the app files

---
+++ script.js
@@ -0,0 +1,49 @@
+var record = 0;
+var last = 0;
+var output = document.querySelector('output');
+var button = document.querySelector('button');
+var na = output.textContent;
+var isTouchDevice =
+	('ontouchstart' in window) ||
+	!!(window.DocumentTouch && document instanceof DocumentTouch);
+var tapEvent = isTouchDevice ? 'touchstart' : 'mousedown';
+var tapReleaseEvent = isTouchDevice ? 'touchend' : 'mouseup';
+var wasFirstTapReleased;
+
+function setRecord(now) {
+	record = now - last;
+	output.textContent = record;
+}
+
+function tapRelease() {
+	wasFirstTapReleased = true;
+}
+
+function tap(event) {
+	if (event.target === button) return;
+	event.preventDefault();
+	if (!isTouchDevice && event.button !== 0) return;
+	var now = Date.now();
+	if (record) {
+		if (now - last < record && wasFirstTapReleased) setRecord(now);
+	} else {
+		if (last && wasFirstTapReleased) setRecord(now);
+	}
+	last = now;
+	wasFirstTapReleased = false;
+	window.addEventListener(tapReleaseEvent, tapRelease, false);
+}
+
+function reset() {
+	record = last = 0;
+	output.textContent = na;
+	wasFirstTapReleased = false;
+}
+
+function preventDefault(event) {
+	event.preventDefault();
+}
+
+button.addEventListener('click', reset, false);
+window.addEventListener(tapEvent, tap, false);
+document.body.addEventListener('touchmove', preventDefault, false);
