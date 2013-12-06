579057ec5096563355c1df72786cbb0f09e13021
Daniel Hug <danielthehug@gmail.com>
2013-12-06 11:37:00 -0800

    handle app updates better, add documentation to script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+// Define starting variables and helper functions:
 var record = 0;
 var last = 0;
-var output = document.querySelector('output');
-var button = document.querySelector('button');
 var isTouchDevice =
 	('ontouchstart' in window) ||
 	!!(window.DocumentTouch && document instanceof DocumentTouch);
@@ -9,16 +8,35 @@ var tapEvent = isTouchDevice ? 'touchstart' : 'mousedown';
 var tapReleaseEvent = isTouchDevice ? 'touchend' : 'mouseup';
 var wasFirstTapReleased;
 
+// addEventListener wrapper:
+function on(target, type, callback) {
+	target.addEventListener(type, callback, false);
+}
+
+// Update record output:
+var output = document.querySelector('output');
 function setRecord(now) {
 	record = now - last;
 	output.textContent = record;
 }
 
+// Ensure a tap release occured between taps:
 function tapRelease() {
 	wasFirstTapReleased = true;
 }
 
-function tap(event) {
+
+// Reset record when reset button is clicked:
+var button = document.querySelector('button');
+on(button, 'click', function() {
+	record = last = 0;
+	output.textContent = '';
+	wasFirstTapReleased = false;
+});
+
+
+// Listen for tap event:
+on(window, tapEvent, function tap(event) {
 	if (event.target === button) return;
 	event.preventDefault();
 	if (!isTouchDevice && event.button !== 0) return;
@@ -31,18 +49,22 @@ function tap(event) {
 	last = now;
 	wasFirstTapReleased = false;
 	window.addEventListener(tapReleaseEvent, tapRelease, false);
-}
+});
 
-function reset() {
-	record = last = 0;
-	output.textContent = '';
-	wasFirstTapReleased = false;
-}
 
-function preventDefault(event) {
+// prevent default touch swipe action:
+on(document.body, 'touchmove', function(event) {
 	event.preventDefault();
-}
+});
 
-button.addEventListener('click', reset, false);
-window.addEventListener(tapEvent, tap, false);
-document.body.addEventListener('touchmove', preventDefault, false);
+
+// Listen for app updates:
+var appCache = window.applicationCache
+on(appCache, 'updateready', function(e) {
+	if (appCache.status == appCache.UPDATEREADY) {
+		appCache.swapCache();
+		if (confirm('A new version of this webapp has been loaded. Update now?')) {
+			window.location.reload();
+		}
+	}
+});
