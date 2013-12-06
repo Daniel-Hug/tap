ba5883055726211b74cd11e871b4b911db6b553f
Daniel Hug <danielthehug@gmail.com>
2013-12-06 12:04:51 -0800

    improve app intuitiveness, add title attributes for non touch devices

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // Define starting variables and helper functions:
-var record = 0;
-var last = 0;
 var isTouchDevice =
 	('ontouchstart' in window) ||
 	!!(window.DocumentTouch && document instanceof DocumentTouch);
 var tapEvent = isTouchDevice ? 'touchstart' : 'mousedown';
 var tapReleaseEvent = isTouchDevice ? 'touchend' : 'mouseup';
-var wasFirstTapReleased;
+var wasFirstTapReleased, record, last;
 
 // addEventListener wrapper:
 function on(target, type, callback) {
@@ -17,7 +15,8 @@ function on(target, type, callback) {
 var output = document.querySelector('output');
 function setRecord(now) {
 	record = now - last;
-	output.textContent = record;
+	output.textContent = record + 'ms';
+	if (!isTouchDevice) output.title = 'milliseconds between your quickest taps';
 }
 
 // Ensure a tap release occured between taps:
@@ -26,18 +25,21 @@ function tapRelease() {
 }
 
 
-// Reset record when reset button is clicked:
-var button = document.querySelector('button');
-on(button, 'click', function() {
+// Reset record:
+var resetBtn = document.querySelector('button');
+function reset() {
 	record = last = 0;
 	output.textContent = '';
+	if (!isTouchDevice) output.title = 'Tap away!';
 	wasFirstTapReleased = false;
-});
+}
+reset();
+on(resetBtn, 'click', reset);
 
 
 // Listen for tap event:
 on(window, tapEvent, function tap(event) {
-	if (event.target === button) return;
+	if (event.target === resetBtn) return;
 	event.preventDefault();
 	if (!isTouchDevice && event.button !== 0) return;
 	var now = Date.now();
