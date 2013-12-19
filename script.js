9975e5d646093da868bb1521747d75c6bb278b5d
Daniel Hug <danielthehug@gmail.com>
2013-12-18 22:22:19 -0800

    Add bar graph of tap history

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-// Define starting variables and helper functions:
 var isTouchDevice =
 	('ontouchstart' in window) ||
 	!!(window.DocumentTouch && document instanceof DocumentTouch);
-var tapEvent = isTouchDevice ? 'touchstart' : 'mousedown';
-var tapReleaseEvent = isTouchDevice ? 'touchend' : 'mouseup';
-var wasFirstTapReleased, record, last;
+var prevTapWasReleased, record, last, highest, taps;
+
+function arrMax(array) {
+	return Math.max.apply(null, array);
+}
 
 // addEventListener wrapper:
 function on(target, type, callback) {
 	target.addEventListener(type, callback, false);
 }
 
-// Update record output:
-var output = document.querySelector('output');
-function setRecord(now) {
-	record = now - last;
-	output.textContent = record + 'ms';
-	if (!isTouchDevice) output.title = 'milliseconds between your quickest taps';
-}
 
-// Ensure a tap release occured between taps:
-function tapRelease() {
-	wasFirstTapReleased = true;
+// Update record:
+var recordEl = document.getElementById('record');
+function setRecord(lapsed) {
+	record = lapsed;
+	recordEl.textContent = record + 'ms';
 }
 
 
-// Reset record:
-var resetBtn = document.querySelector('button');
+// Update history:
+var historyEl = document.getElementById('history');
+var bars = historyEl.children;
+function makeHistory(lapsed) {
+	if (bars.length >= 10) {
+		historyEl.removeChild(bars[0]);
+		taps.shift();
+	}
+
+	var li = document.createElement('li');
+	li.textContent = lapsed;
+
+	taps.push(lapsed);
+	if ( highest !== (highest = arrMax(taps)) ) {
+		[].forEach.call(bars, function(el, i) {
+			el.style.height = taps[i]/highest*100 + '%';
+		});
+	}
+	li.style.height = lapsed/highest*100 + '%';
+	historyEl.appendChild(li);
+}
+
+
 function reset() {
-	record = last = 0;
-	output.textContent = '';
-	if (!isTouchDevice) output.title = 'Tap away!';
-	wasFirstTapReleased = false;
+	record = last = highest = 0;
+	taps = [];
+	recordEl.textContent = '';
+	prevTapWasReleased = false;
+	historyEl.innerHTML = '';
 }
 reset();
+var resetBtn = document.getElementById('reset');
 on(resetBtn, 'click', reset);
 
 
 // Listen for tap event:
+var tapEvent = isTouchDevice ? 'touchstart' : 'mousedown';
+var tapReleaseEvent = isTouchDevice ? 'touchend' : 'mouseup';
 on(window, tapEvent, function tap(event) {
 	if (event.target === resetBtn) return;
 	event.preventDefault();
 	if (!isTouchDevice && event.button !== 0) return;
 	var now = Date.now();
-	if (record) {
-		if (now - last < record && wasFirstTapReleased) setRecord(now);
-	} else {
-		if (last && wasFirstTapReleased) setRecord(now);
+	if (prevTapWasReleased) {
+		var lapsed = now - last;
+		if (!record || lapsed < record) setRecord(lapsed);
+		makeHistory(lapsed);
 	}
 	last = now;
-	wasFirstTapReleased = false;
+	prevTapWasReleased = false;
 	window.addEventListener(tapReleaseEvent, tapRelease, false);
 });
+
+function tapRelease() {
+	prevTapWasReleased = true;
+}
 
 
 // prevent default touch swipe action:
@@ -61,7 +86,7 @@ on(document.body, 'touchmove', function(event) {
 
 
 // Listen for app updates:
-var appCache = window.applicationCache
+var appCache = window.applicationCache;
 on(appCache, 'updateready', function(e) {
 	if (appCache.status == appCache.UPDATEREADY) {
 		appCache.swapCache();
@@ -69,4 +94,9 @@ on(appCache, 'updateready', function(e) {
 			window.location.reload();
 		}
 	}
+});
+
+// Trigger repaint for viewport units:
+on(window, 'resize', function() {
+	document.body.removeChild(document.body.appendChild(document.createElement('style')));
 });
